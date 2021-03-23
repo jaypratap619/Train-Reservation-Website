80750eec7ecd335b552f6ca0f609251679a3ea57
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2021-03-23 16:21:01 +0530

    Update user.js

--- internproj/models/user.js
+++ internproj/models/user.js
@@ -20,16 +20,8 @@ const userSchema = new mongoose.Schema({
     },
     salt:String,
 },{timestamps:true});
-
-userSchema.virtual("password")
-    .set(function(password){
-        this._password=password
-        this.salt=uuidv1();
-        this.encry_password = this.securePassword(password)
-    })
-    .get(function(){
-        return this._password
-    })
+//vit
+//all
 
 userSchema.methods ={
 
